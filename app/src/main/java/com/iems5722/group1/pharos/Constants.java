98dc17ac65e8fa2e93448fb05b9bde06151248b9
SoraCaptain <ouentomoeien@gmail.com>
2017-04-03 00:52:40 +0800

    First copy

---
+++ app/src/main/java/com/iems5722/group1/pharos/Constants.java
@@ -0,0 +1,11 @@
+package com.iems5722.group1.pharos;
+
+/**
+ * Created by Kevin on 2016/11/28.
+ * Blog:http://blog.csdn.net/student9128
+ * Description:
+ */
+
+public class Constants {
+    public static final String ARGS = "args";
+}
---
+++ app/src/main/java/com/iems5722/group1/pharos/fragment/subfragment/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.iems5722.group1.pharos.fragment.subfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.iems5722.group1.pharos.Constants;
+import com.iems5722.group1.pharos.R;
+
+/**
+ * Created by Kevin on 2016/11/28.
+ * Blog:http://blog.csdn.net/student9128
+ * Description: HomeFragment
+ */
+
+public class HomeFragment extends Fragment {
+    public static HomeFragment newInstance(String s){
+        HomeFragment homeFragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.ARGS,s);
+        homeFragment.setArguments(bundle);
+        return homeFragment;
+}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sub_content, container, false);
+        Bundle bundle = getArguments();
+        String s = bundle.getString(Constants.ARGS);
+        TextView textView = (TextView) view.findViewById(R.id.fragment_text_view);
+        textView.setText(s);
+        return view;
+    }
+}
